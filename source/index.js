8786b028aa2495e4ae3ee49eed361f1d67c8a88d
rogerbf <roger@bergfalk.nu>
2017-03-08 21:50:22 +0100

    upgrade dependency, refactor

--- source/index.js
+++ source/index.js
@@ -1,5 +1,7 @@
-const args = require(`options-to-args`)
+const aliases = require(`./aliases`)
+const args = require(`options-to-args`).alias(aliases)
+
 const { execFile } = require(`child_process`)
 const sips = require(`./sips`)
 
-module.exports = sips.bind(null, { args, execFile })
+module.exports = sips.bind(null, { execFile, args })
--- source/sips.js
+++ source/sips.js
@@ -2,34 +2,7 @@ module.exports = ({ execFile, args }, options) =>
   new Promise((resolve, reject) => {
     execFile(
       `sips`,
-      args
-      .addAlias({ getProperty: `g` })
-      .addAlias({ extractTag: `X` })
-      .addAlias({ verify: `v` })
-      .addAlias({ extractProfile: `x` })
-      .addAlias({ setProperty: `s` })
-      .addAlias({ deleteProperty: `d` })
-      .addAlias({ deleteTag: `-deleteTag` })
-      .addAlias({ copyTag: `-copyTag` })
-      .addAlias({ loadTag: `-loadTag` })
-      .addAlias({ repair: `-repair` })
-      .addAlias({ embedProfile: `e` })
-      .addAlias({ embedProfileIfNone: `E` })
-      .addAlias({ matchTo: `m` })
-      .addAlias({ matchToWithIntent: `M` })
-      .addAlias({ deleteColorManagementProperties: `-deleteColorManagementProperties` })
-      .addAlias({ rotate: `r` })
-      .addAlias({ flip: `f` })
-      .addAlias({ cropToHeightWidth: `c` })
-      .addAlias({ padToHeightWidth: `p` })
-      .addAlias({ padColor: `-padColor` })
-      .addAlias({ resampleHeightWidth: `z` })
-      .addAlias({ resampleWidth: `-resampleWidth` })
-      .addAlias({ resampleHeight: `-resampleHeight` })
-      .addAlias({ resampleHeightWidthMax: `Z` })
-      .addAlias({ addIcon: `i` })
-      .addAlias({ optimizeColorForSharing: `o` })
-      .addAlias({ help: `-help` })(options),
+      args(options),
       (error, stdout, stderr) => {
         error
         ? reject(error)
