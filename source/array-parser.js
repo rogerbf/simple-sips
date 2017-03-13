46edfc7a776a70835953ca2482897c1a0eeee6d4
rogerbf <roger@bergfalk.nu>
2017-03-13 22:46:33 +0100

    clarification

--- source/array-parser.js
+++ source/array-parser.js
@@ -1,2 +1,2 @@
-module.exports = ({ prefix, option, value }) =>
-  value.reduce((acc, v) => acc.concat([ prefix + option, v ]), [])
+module.exports = ({ parse, prefix, option, value }) =>
+  value.reduce((acc, v) => acc.concat(prefix + option).concat(v), [])
